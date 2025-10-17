6def6f4e78abcc7c0108fb090b7cf1274afe8cd1
Showrab15 <supto50showrab@gmail.com>
2025-10-17 20:52:45 +0600

    server first setup complete

--- server/index.js
+++ server/index.js
@@ -15,6 +15,27 @@ const io = new Server(server, {
   },
 });
 
+io.on("connection", (socket) => {
+  console.log("user connected", socket.id);
+
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined ${room}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("typing", ({ username, room }) => {
+    socket.to(data.room).emit("user_typing", username);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+});
+
 server.listen(3000, () => {
   console.log("The server port is running on 3000");
 });
